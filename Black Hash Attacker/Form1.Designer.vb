97dc11978472de4b456518802219a06f9b0a432e
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-26 20:42:35 +0100

    Black-Hash-Attacker-SRC
    Black-Hash-Attacker-SRC

---
+++ Black Hash Attacker/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(349, 226)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.CheckBox2)
+        Me.TabPage1.Controls.Add(Me.TextBox4)
+        Me.TabPage1.Controls.Add(Me.TextBox3)
+        Me.TabPage1.Controls.Add(Me.ComboBox1)
+        Me.TabPage1.Controls.Add(Me.Button2)
+        Me.TabPage1.Controls.Add(Me.TextBox2)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(341, 200)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Single Attack"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 153)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(121, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Password Tested: None"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(244, 65)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(59, 17)
+        Me.CheckBox2.TabIndex = 13
+        Me.CheckBox2.Text = "Enable"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Enabled = False
+        Me.TextBox4.Location = New System.Drawing.Point(6, 61)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(232, 20)
+        Me.TextBox4.TabIndex = 12
+        Me.TextBox4.Text = "Salt"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(8, 174)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(327, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"MD5", "SHA1", "SHA256", "SHA384", "SHA512"})
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 87)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(232, 21)
+        Me.ComboBox1.TabIndex = 10
+        Me.ComboBox1.Text = "Select Hash Type"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(244, 35)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "[ . . . . . ]"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(6, 9)
+        Me.TextBox2.MaxLength = 999999999
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(232, 20)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.Text = "Enter Encrypted Hash"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(6, 35)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(232, 20)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "Select Password List"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(8, 114)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(327, 36)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "[ Attack it ]"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.Label2)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(341, 200)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "About Us"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(3, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(222, 150)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = resources.GetString("Label2.Text")
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(349, 226)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Black Bruteforce Attack Tool v0.1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Black Hash Attacker/Form1.vb
@@ -0,0 +1,166 @@
+﻿Imports System.Security.Cryptography
+Imports System.Text
+Public Class Form1
+    Dim salt As String = ""
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        If CheckBox2.Checked Then
+            salt = TextBox4.Text
+        End If
+        TextBox3.Text = "Please Wait..."
+        Dim attacker As New Threading.Thread(AddressOf SingleAttack)
+        attacker.Start(ComboBox1.Text)
+    End Sub
+
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim a As New OpenFileDialog
+        With a
+            .Filter = "txt (*.txt)|*.txt"
+            .Title = "select password list"
+            If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                TextBox1.Text = .FileName
+            Else
+                Return
+            End If
+        End With
+    End Sub
+
+    Private Sub TextBox2_TextChanged_1(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If TextBox2.Text.Length = 32 Then
+            ComboBox1.Text = "MD5"
+        ElseIf TextBox2.Text.Length = 40 Then
+            ComboBox1.Text = "SHA1"
+        ElseIf TextBox2.Text.Length = 64 Then
+            ComboBox1.Text = "SHA256"
+        ElseIf TextBox2.Text.Length = 96 Then
+            ComboBox1.Text = "SHA384"
+        ElseIf TextBox2.Text.Length = 128 Then
+            ComboBox1.Text = "SHA512"
+        ElseIf TextBox2.Text.Length > 128 Then
+            MsgBox("Sorry we do not support this hash yet", MsgBoxStyle.Critical, "):")
+        End If
+    End Sub
+    Private Delegate Sub UpdatePasswordTextDelegate(ByVal status As String)
+    Private Sub UpdatePasswordText(ByVal status As String)
+        Try
+            BeginInvoke(New UpdatePasswordTextDelegate(AddressOf UpdatePasswordTextSub), status)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub UpdatePasswordTextSub(ByVal status As String)
+        Try
+            TextBox3.Text = status
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Sub SingleAttack(ByVal HashType As String)
+        Dim PasswordList() As String = IO.File.ReadAllLines(TextBox1.Text)
+        Dim Count As Integer = PasswordList.Length
+        Dim Password As Integer = 0
+        For Each hash As String In PasswordList
+            Select Case HashType
+                Case "MD5"
+                    Label1.Text = "Password Teste: " & Password & "/" & Count
+                    Password = Password + 1
+                    If CheckBox2.Checked Then
+                        If GetHashMD5(salt & hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    Else
+                        If GetHashMD5(hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    End If
+
+                Case "SHA1"
+                    Label1.Text = "Password Teste: " & Password & "/" & Count
+                    Password = Password + 1
+                    If CheckBox2.Checked Then
+                        If GetHashSHA1(salt & hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    Else
+                        If GetHashSHA1(hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    End If
+
+                Case "SHA256"
+                    Label1.Text = "Password Teste: " & Password & "/" & Count
+                    Password = Password + 1
+                    If CheckBox2.Checked Then
+                        If GetHashSHA256(salt & hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    Else
+                        If GetHashSHA256(salt & hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    End If
+
+
+                Case "SHA384"
+                    Label1.Text = "Password Teste: " & Password & "/" & Count
+                    Password = Password + 1
+                    If CheckBox2.Checked Then
+                        If GetHashSHA384(salt & hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    Else
+                        If GetHashSHA384(hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    End If
+
+
+                Case "SHA512"
+                    Label1.Text = "Password Teste: " & Password & "/" & Count
+                    Password = Password + 1
+                    If CheckBox2.Checked Then
+                        If GetHashSHA512(salt & hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    Else
+                        If GetHashSHA512(hash) = TextBox2.Text.ToUpper Then
+                            UpdatePasswordText("Password Found : " & hash)
+                            MsgBox("Hash Has Been Cracked !", MsgBoxStyle.Information, "Done!")
+                            Return
+                        End If
+                    End If
+            End Select
+        Next
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("old var اصدار قديم")
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            CheckBox2.Text = "Disable"
+            TextBox4.Enabled = True
+        Else
+            CheckBox2.Text = "Enable"
+            TextBox4.Enabled = False
+        End If
+    End Sub
+End Class
